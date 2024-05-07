780d9cfb9c7ddd9c7c4e36f0c2cf64a55f78f847
ramesvar <ramesvarjobsearch@gmail.com>
2024-05-07 02:09:11 +0200

    Initial commit

---
+++ src/main/java/org/BITS/Assignement_Jenkins/Assignment2/Application.java
@@ -0,0 +1,8 @@
+package org.BITS.Assignement_Jenkins.Assignment2;
+
+public class Application {
+
+    public void add(int a, int b) {
+        System.out.println("addition is" + a+b);
+    }
+}
---
+++ src/test/java/org/BITS/Assignement_Jenkins/Assignment2/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.BITS.Assignement_Jenkins.Assignment2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
